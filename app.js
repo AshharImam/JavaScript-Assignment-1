3591e75f98a86316a3808915bd533b9c940cae64
Ashhar Imam <ashharimam490@.com>
2020-06-14 22:14:31 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,188 @@
+// ================CHAPTER 01 Q#01
+
+// alert("Hello, World");
+
+// ================CHAPTER 01 Q#02
+
+// alert("Error! Please enter a valid password.");
+
+// ================CHAPTER 01 Q#03
+
+// alert("Welcome to JS Land...\nHappy coding");
+
+// ================CHAPTER 01 Q#04
+
+// alert("Welcome to JS Land...");
+// alert("Happy coding");
+
+// ================CHAPTER 01 Q#05
+
+// console.log(alert('Hello, I can run JS through my web browser\'s console.'));
+
+// xxxxxxxxxxxxxxxxCHAPTER 01xxxxxxxxxxxxxxxx
+
+
+
+// xxxxxxxxxxxxxxxxCHAPTER 02xxxxxxxxxxxxxxxx
+
+// ================CHAPTER 02 Q#01
+
+// var userName;
+
+// ================CHAPTER 02 Q#02
+
+// var myName = "Syed Muhammad Ashhar Imam";
+
+// ================CHAPTER 02 Q#03
+
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// ================CHAPTER 02 Q#04
+
+// var studentName, years, coarse;
+
+// studentName = "John Doe";
+// years = "15 years old";
+// coarse = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(years);
+// alert(coarse);
+
+// ================CHAPTER 02 Q#05
+
+// var pizza, res;
+// res= "";
+// pizza = "Pizza";
+// for (var i=pizza.length; i>=0; i--){
+//     res +=  pizza.slice(0,i) + "\n";
+// }
+// alert(res);
+
+// ================CHAPTER 02 Q#06
+
+// var email;
+// email = "ashharimam490@gmail.com";
+// alert("My email address is "+email);
+
+// ================CHAPTER 02 Q#07
+
+// var book;
+// book = "A smarter way to learn JavaScript";
+// alert("I\'m trying to learn from book "+book);
+
+
+// ================CHAPTER 02 Q#08
+
+// var txt = "<h1>Yah! I can write HTML content through JavaScript</h1>";
+// document.write(txt);
+
+// ================CHAPTER 02 Q#08
+
+// var str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(str);
+
+// xxxxxxxxxxxxxxxxCHAPTER 02xxxxxxxxxxxxxxxx
+
+
+
+// xxxxxxxxxxxxxxxxCHAPTER 03xxxxxxxxxxxxxxxsx
+
+// ================CHAPTER 03 Q#01
+
+// var age=15;
+// alert("I am "+age+" years old");
+
+// ================CHAPTER 03 Q#03
+
+// var birthYear = 1998, txt1 = "<h1>My birth year is "+ birthYear +"</h1>", txt2 = "<h1>Data type of declared valiable is "+ typeof(birthYear) +"</h1>";
+
+// document.write(txt1);
+// document.write(txt2);
+
+// ================CHAPTER 03 Q#04
+
+// var name, product, quantity, str;
+// name = prompt("Enter your name:");
+// product = prompt("Enter product title:");
+// quantity = prompt("Enter product quantity:");
+// str = "<h1>"+ name + " ordered "+ quantity + " " + product + " on XYZ Clothing Store.</h1>"
+// document.write(str);
+
+// ================CHAPTER 03 Q#05
+
+// xxxxxxxxxxxxxxxxCHAPTER 03xxxxxxxxxxxxxxxsx
+
+
+
+
+// xxxxxxxxxxxxxxxxCHAPTER 04xxxxxxxxxxxxxxxsx
+
+// ================CHAPTER 04 Q#01
+
+// var name="John", class="Mobile App", day="Sunday";
+
+// ================CHAPTER 04 Q#02
+
+// var name, _last, sun_day, $class, $class_name;
+// var 1name, *class, /sunday, class*name;
+
+// ================CHAPTER 04 Q#03
+
+// document.write("<h2>Rules for naming JS variables</h2>")
+// document.write("Variable names can only contain numbers, $, alphabet and _")
+// document.write("Variables must begin with a letter, $ or _")
+// document.write("Variable names are case sensitive")
+// document.write("Variable names should not be JS keyword")
+
+// xxxxxxxxxxxxxxxxCHAPTER 04xxxxxxxxxxxxxxxsx
+
+
+
+// xxxxxxxxxxxxxxxxCHAPTER 05xxxxxxxxxxxxxxxsx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
