fa8da0c4c790fd001bec15e41af2b70b6048e62c
Ashhar Imam <ashharimam490@gmail.com>
2020-07-16 13:01:28 +0500

    12-13

---
+++ ch12-13.js
@@ -0,0 +1,88 @@
+// ================ Q#01
+
+// var input, char;
+// input = prompt("Enter a character");
+// char = input.charCodeAt(0);
+// if (char >= 65 && char <= 90) {
+//     document.write("<p>Upper case letter</p>")
+// } else if (char >= 97 && char <= 122) {
+//     document.write("<p>lower case letter</p>")
+// } else if (char >= 48 && char <= 57) {
+//     document.write("<p>number</p>")
+// }
+
+// ================ Q#02
+
+// var f, s;
+
+// f = parseInt(prompt("Enter 1st number"))
+// s = parseInt(prompt("Enter 2nd number"))
+// if (f > s) {
+//     document.write(`<p>${f}</p>`)
+
+// } else if (s > f) {
+//     document.write(`<p>${s}</p>`)
+// }
+
+// ================ Q#03
+
+// var f;
+
+// f = parseInt(prompt("Enter 1st number"))
+// if (f > 0) {
+//     document.write(`<p>number is positive</p>`)
+
+// } else if (f < 0) {
+//     document.write(`<p>number is negative</p>`)
+// }else if (f === 0) {
+//     document.write(`<p>number is zero</p>`)
+// }
+
+// ================ Q#04
+
+// var  char;
+// char = prompt("Enter an alphabet");
+// if (char === 'a' || char === 'e' || char === 'i'|| char === 'o'|| char === 'u') {
+//     document.write(`<p>True</p>`)
+// } else if (char >= 97 && char <= 122) {
+//     document.write(`<p>False</p>`)
+// }
+
+// ================ Q#05
+
+// var char, password;
+// password = 'abc123'
+// char = prompt("Enter password");
+// if (char === '') {
+//     document.write(`<p>Please Enter Your Password</p>`)
+// } else if (char === password) {
+//     document.write(`<p>Correct Password</p>`)
+// } else if (char !== password) {
+//     document.write(`<p>Incorrect Password</p>`)
+// }
+
+// ================ Q#06
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+
+// ================ Q#07
+
+// var time;
+// time = parseInt(prompt("Enter current time"));
+
+// if (time >= 0000 && time < 1200) {
+//     document.write(`<p>Good morning</p>`)
+// } else if (time >= 1200 && time < 1700) {
+//     document.write(`<p>Good afternoon</p>`)
+// } else if (time >= 1700 && time < 2100) {
+//     document.write(`<p>Good evening</p>`)
+// } else if (time >= 2100 && time < 2359) {
+//     document.write(`<p>Good afternoon</p>`)
+// }
+
