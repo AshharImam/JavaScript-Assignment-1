efd0b5b566d3ce5fa854713d92b6769f9ac8dc3e
Ashhar Imam <ashharimam490@gmail.com>
2020-07-19 12:17:20 +0500

    ch17-20

---
+++ ch17-20.js
@@ -0,0 +1,86 @@
+// ================ Q#1
+
+// var mulDimArr = [[]];
+
+// ================ Q#2
+
+// var mulDimArr = [[0, 1, 2, 3], [1, 0, 1, 2], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+// ================ Q#3
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(`<p>${i}</p>`)
+// }
+
+// ================ Q#4
+
+// var table, len;
+// table = Number(prompt("Enter number to print it's multiplication table"));
+// len = Number(prompt("Enter the length of multiplication table"));
+// for (let index = 1; index <= len; index++) {
+    // document.write(`<p>${table} x ${index} = ${table*index}</p>`)
+// }
+
+// ================ Q#5
+
+// var arr = ["apple", "bnana", "mango", "orange", "strawberry"];
+// for (var i in arr) {
+    // document.write(`<p>${arr[i]}</p>`)
+// }
+
+// ================ Q#6
+
+// for (var i = 1; i <=15; i++) {
+//     document.write(`<p>${i}</p>`)
+
+// }
+// for (var i = 10; i >0; i--) {
+//     document.write(`<p>${i}</p>`)
+
+// }
+
+// for (var i = 0; i <= 20; i+=2) {
+//     document.write(`<p>${i}</p>`)
+    
+// }
+// for (var i = 1; i <= 19; i+=2) {
+//     document.write(`<p>${i}</p>`)
+    
+// }
+// for (var i = 2000; i <= 20000; i+=2000) {
+    // document.write(`<p>${i}</p>`)
+    
+// }
+
+// ================ Q#7
+
+// var item = ['bun', 'pastery', 'juice', 'butter'];
+// var search = prompt("Search in the bakery item");
+// if (item.indexOf(search) !== -1) {
+//     i = item.indexOf(search);
+    // document.write(`<p>${search} is available at index ${i}</p>`);
+// }else {
+    // document.write(`<p>Sorry this item is not available</p>`);
+// }
+
+// ================ Q#8
+
+// var A = [24, 53, 78, 91, 12];
+// var l=A[0];
+// for (var i in A) {
+//     if (A[i] > l) {
+//         l = A[i];
+//     }
+// }
+// document.write(`<p>${l}</p>`);
+
+// ================ Q#9
+
+// var A = [24, 53, 78, 91, 12];
+// var l=A[0];
+// for (var i in A) {
+//     if (A[i] < l) {
+//         l = A[i];
+//     }
+// }
+// document.write(`<p>${l}</p>`);
